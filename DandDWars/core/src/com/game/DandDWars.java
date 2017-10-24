aef062e89feecf0ffcd94691544e3b4386eecb69
Aztec50 <32782471+Aztec50@users.noreply.github.com>
2017-10-24 16:08:34 -0700

    HUD started
    started making a hud that shouuuuld be able to be given any troop and draw its stats and position

--- DandDWars/core/src/com/game/DandDWars.java
+++ DandDWars/core/src/com/game/DandDWars.java
@@ -16,6 +16,7 @@ import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import com.badlogic.gdx.math.Vector2;
 
@@ -23,16 +24,26 @@ import com.badlogic.gdx.utils.Array;
 
 import com.mygdx.game.objects.Troop;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
 public class DandDWars extends ApplicationAdapter implements InputProcessor {
 	
 	OrthographicCamera camera;
+	
 	TiledMap tiledMap;
 	TiledMapRenderer tiledMapRenderer;
 	
 	SpriteBatch batch;
+	ShapeRenderer sr;
+	BitmapFont font;
+	
 	String currentMap;
+	
+	
 	
 	Troop troop;
 	Troop troop2;
@@ -53,6 +64,9 @@ public class DandDWars extends ApplicationAdapter implements InputProcessor {
 		Gdx.input.setInputProcessor(this);
 		
 		batch = new SpriteBatch();
+		font = new BitmapFont();
+		font.setColor(Color.RED);
+		sr = new ShapeRenderer();
 	
 		currentMap = "maps/GrassMap.tmx";
 	
@@ -91,7 +105,56 @@ public class DandDWars extends ApplicationAdapter implements InputProcessor {
 		batch.begin();
 		troop.render(batch);
 		troop2.render(batch);
+		drawHUD(troop);
 		batch.end();
+	}
+	
+	public void drawHUD(Troop currTroop) {
+		
+		//draw big rectangle
+		batch.end();
+		sr.begin(ShapeType.Filled);
+		sr.setColor(Color.WHITE);
+		sr.rect(0, 0, 120, 120);
+		sr.end();
+		batch.begin();
+		//draw troop name
+		switch(currTroop.troopType) {
+				case KNIGHT: {
+					font.draw(batch, "Knight", 40, 110);
+					
+
+		TextureRegion reg = null;
+		reg = troop.animation.getKeyFrame(troop.stateTime,true);
+
+		/*  This draw function was a doozy to figure out, but as it is
+         *  it should draw and animate all of our units. In the case that
+         *  something needs to be changed, the prototype is provided. 
+		 *
+         *  draw(Texture texture, float x, float y, float width, float height,
+		 *       int srcX, int srcY,
+		 *		 int srcWidth, int srcHeight,
+		 *		 boolean flipX, boolean flipY);
+		 */
+		
+		batch.draw(reg.getTexture(), 44, 60, 32, 32,
+				   reg.getRegionX(), reg.getRegionY(),
+				   reg.getRegionWidth(), reg.getRegionHeight(),
+				   false, false);
+					break;
+				}
+				case ARCHER: {
+					font.draw(batch, "Archer", 30, 110);
+					break;
+				}
+				case WIZARD: {
+					font.draw(batch, "Wizard", 30, 110);
+					break;
+				}
+		}
+		//draw troop scaled up over current tile
+		//draw stats of said troop
+		//font.draw(batch, "PAUSED", width/2, height/2, width/2, 10, false);
 	}
 	
 	@Override
