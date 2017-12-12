5c4ddfc0603e6d6bb6993616ad86449191c69b2c
JamesMYoung <youn5393@vandals.uidaho.edu>
2017-12-12 12:24:17 -0800

    Recover Attempt

---
+++ Battle-for-Grinheldt/core/src/com/game/ai/Node.java
@@ -0,0 +1,41 @@
+package com.mygdx.game.ai;
+
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.ai.pfa.Connection;
+import com.mygdx.game.ai.ConnectionImp;
+
+public class Node {
+	private Array<Connection<Node>> connections = new Array<Connection<Node>>();
+	public int type;
+	public int x;
+	public int y;
+	public int index;
+	public int moveCost;
+	
+	public Node(int x, int y, int mv) {
+		index = Node.Indexer.getIndex();
+		this.x = x;
+		this.y = y;
+		moveCost = mv;
+	}
+	
+	public int getIndex() {
+		return index;
+	}
+	
+	public Array<Connection<Node>> getConnections() {
+		return connections;
+	}
+	
+	public void createConnection( Node toNode, float cost) {
+		connections.add(new ConnectionImp(this, toNode, cost));
+	}
+	
+	public static class Indexer {
+		private static int index = 0;
+		
+		public static int getIndex() {
+			return index++;
+		}
+	}
+}
